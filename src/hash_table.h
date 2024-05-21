24b8ccfb2df086658f21160af5d4a0a3c24a65e5
lajesfen <luciano.aguirre@utec.edu.pe>
2024-05-21 17:35:21 -0500

    add: Hash Table extra functions

--- src/hash_table.h
+++ src/hash_table.h
@@ -9,9 +9,7 @@ struct Node {
     int value;
     Node *next;
 
-    Node(char k, int v) {
-        value = v;
-        key = k;
+    Node() {
         next = nullptr;
     }
 };
@@ -21,22 +19,20 @@ private:
     Node **table;
     int size;
 
-    static int hash(char key) { // Modify depending on Hashing method
+    static int hash(char key) {
         return key - 'a';
     }
 
 public:
-    HashTable(int t) : size(t) {
+    explicit HashTable(int t) : size(t) {
         size = std::max(0, std::min(size, MAX_SIZE));
         table = new Node *[size]();
     }
 
     ~HashTable() {
-        for (int i = 0; i < size; ++i)
-        {
+        for (int i = 0; i < size; ++i) {
             Node *current = table[i];
-            while (current != nullptr)
-            {
+            while (current != nullptr) {
                 Node *temp = current;
                 current = current->next;
                 delete temp;
@@ -47,28 +43,32 @@ public:
 
     void insert(char key, int value) {
         int i = hash(key) % size;
-        Node *new_node = new Node(key, value);
+        Node *newNode = new Node();
+        newNode->key = key;
+        newNode->value = value;
 
-        if (table[i] == nullptr)
-            table[i] = new_node;
-        else {
+        if (table[i] == nullptr) {
+            table[i] = newNode;
+        } else {
             Node *current = table[i];
             while (current->next != nullptr) {
                 if (current->key == key) {
                     current->value = value;
+                    delete newNode;
                     return;
                 }
                 current = current->next;
             }
             if (current->key == key) {
                 current->value = value;
+                delete newNode;
             } else {
-                current->next = new_node;
+                current->next = newNode;
             }
         }
     }
 
-    int find(char key) {
+    int get(char key) {
         int i = hash(key) % size;
         Node *current = table[i];
         while (current != nullptr) {
@@ -78,6 +78,54 @@ public:
             current = current->next;
         }
         return 0;
+    }
+
+    void remove(char key) {
+        int i = hash(key) % size;
+        Node *current = table[i];
+        Node *prev = nullptr;
+
+        while (current != nullptr && current->key != key) {
+            prev = current;
+            current = current->next;
+        }
+
+        if (current == nullptr) {
+            return;
+        }
+
+        if (prev == nullptr) {
+            table[i] = current->next;
+        } else {
+            prev->next = current->next;
+        }
+
+        delete current;
+    }
+
+    void update(char key, int value) {
+        int i = hash(key) % size;
+        Node *current = table[i];
+        while (current != nullptr) {
+            if (current->key == key) {
+                current->value = value;
+                return;
+            }
+            current = current->next;
+        }
+        insert(key, value);
+    }
+
+    void clear() {
+        for (int i = 0; i < size; ++i) {
+            Node *current = table[i];
+            while (current != nullptr) {
+                Node *temp = current;
+                current = current->next;
+                delete temp;
+            }
+            table[i] = nullptr;
+        }
     }
 };
 
